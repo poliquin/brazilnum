17dd31396200c38bf1ead5f4e53e0178388dd691
Chris Poliquin <chrispoliquin@gmail.com>
2016-03-16 21:12:43 -0400

    Update setup metadata to mention municipal codes
    This fixes #3.

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
+# -*- coding: utf8 -*-
+
 from distutils.core import setup
 
 setup(
-        name = 'brazilnum',
-        packages = ['brazilnum'],
-        version = '0.8.2',
-        description = 'Validate Brazilian CNPJ, CEI, CPF, PIS/PASEP, and CEP',
-        author = 'Chris Poliquin',
-        author_email = 'cpoliquin@hbs.edu',
-        url = 'https://github.com/poliquin/brazilnum',
-        keywords = ['brazil', 'brasil', 'cnpj', 'cei', 'cpf', 'pis', 'pasep', 'cep'],
-        classifiers = [
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: OS Independent',
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Developers',
-            'Topic :: Utilities'
-            ],
-        long_description = """\
-Validate Brazilian Identification Numbers
------------------------------------------
+    name='brazilnum',
+    packages=['brazilnum'],
+    version='0.8.4',
+    description="Validate Brazilian CNPJ, CEI, CPF, PIS/PASEP, CEP, and municipal numbers",
+    author='Chris Poliquin',
+    author_email='cpoliquin@hbs.edu',
+    url='https://github.com/poliquin/brazilnum',
+    keywords=['brazil', 'brasil', 'cnpj', 'cei', 'cpf', 'pis', 'pasep', 'cep'],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities'
+        ],
+    long_description="""\
+Validate and Parse Brazilian Identification Numbers
+---------------------------------------------------
 
-Python functions for working with CNPJ, CEI, CPF, PIS/PASEP, and CEP numbers,
-which identify firms, people, and places in Brazil.
+Python functions for working with CNPJ, CEI, CPF, PIS/PASEP, CEP, and municipal
+(município) codes, which identify firms, people, and places in Brazil.
 """
 )
